0c0574824b21da7d633d7708f91401ca8331f128
SolovyovKirill <Solowyow.kirya@gmail.com>
2021-11-23 23:11:24 +0300

    DataBinding observe ViewModel

--- app/src/main/java/com/solovyov/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/com/solovyov/composition/presentation/BindingAdapters.kt
@@ -1,10 +1,18 @@
 package com.solovyov.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.solovyov.composition.R
 import com.solovyov.composition.domain.entity.GameResult
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requireAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -57,4 +65,40 @@ private fun getSmileReId(winner: Boolean): Int {
     } else {
         R.drawable.ic_sad
     }
+}
+
+@BindingAdapter("enoughCountOfRightAnswer")
+fun bindEnoughCountOfRightAnswer(textView: TextView, enough: Boolean){
+    textView.setTextColor(getColorByState(textView.context, enough))
+
+}
+
+@BindingAdapter("enoughPercentOfRightAnswer")
+fun bindEnoughPercentOfRightAnswer(progressBar: ProgressBar, enough: Boolean){
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int){
+    textView.text = number.toString()
+
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener){
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+
 }
